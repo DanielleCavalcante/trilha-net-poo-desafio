540110bacf6d0bfb424f66a2183544b62c08fffa
Danielle Cavalcante <daniellesoucavalcante1324@gmail.com>
2024-01-06 14:22:49 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    Console.WriteLine("Smartphone Iphone");
+    Iphone iphone1 = new Iphone("123456789", "iPhone X", "IMEI123", 64);
+    iphone1.Ligar();
+    iphone1.ReceberLigacao();
+    iphone1.InstalarAplicativo("Instagram");
+
+    Console.WriteLine("\nSmartphone Nokia");
+    Nokia nokia1 = new Nokia("111222333", "Nokia 3310", "IMEI456", 128);
+    nokia1.Ligar();
+    nokia1.ReceberLigacao();
+    nokia1.InstalarAplicativo("Twitter");
